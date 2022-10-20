0b1c43164e1c9268936caff0e3764f057155680e
Relinton Pinheiro <relinton.pinheiro.infouniron@gmail.com>
2022-10-19 20:41:43 -0400

    Projeto DDD - Criação da solução, criação das pastas para organização, criação dos projetos

---
+++ SistemaDeVendasVirtual.Application/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SistemaDeVendasVirtual.Application.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
